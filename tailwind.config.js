5df6bc28875a8c948931872925a87aa448212ff9
NickJoin2 <118140011+NickJoin2@users.noreply.github.com>
2023-05-09 07:45:59 +0300

    Website about GPT
    Website about GPT

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html, js}'],
+  theme: {
+    extend: {
+      colors:{
+        accent: '#FF4820',
+        pink: '#AE67FA',
+        text: '#81AFDD',
+        input: '#052D56',
+        block: '#042C54',
+        text_a: '#FF8A71',
+        acya: '#71E5FF',
+        text_black: '#0E0E0E',
+        bg: '#042C54',
+        footer: '#031B34;',
+      },
+    },
+  },
+  plugins: [],
+}
+
